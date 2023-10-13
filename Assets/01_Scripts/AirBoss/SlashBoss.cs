6edce1c16b207f9bf74f7c6efa962a884d44406b
FernandoTFW <fer.aparicio1811@gmail.com>
2023-10-13 19:38:41 -0400

    Solucion temporal slash

--- Assets/01_Scripts/AirBoss/SlashBoss.cs
+++ Assets/01_Scripts/AirBoss/SlashBoss.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slash : MonoBehaviour
+public class SlashBoss : MonoBehaviour
 {
     // Start is called before the first frame update
     int speed = -10;
