86b925646eff03ec4488913013cb7322ba213bc3
FernandoTFW <fer.aparicio1811@gmail.com>
2023-10-13 21:29:36 -0400

    Script de daño

--- Assets/01_Scripts/Enemy.cs
+++ Assets/01_Scripts/Enemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour,IDamageable
 {
     [Header("Atributos")]
     public float life = 3;
