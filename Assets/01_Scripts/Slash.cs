05e6d81e2a01bad796c583d0beb7d551bcde33f9
FernandoTFW <fer.aparicio1811@gmail.com>
2023-10-14 18:58:04 -0400

    Prefab Agua

--- Assets/01_Scripts/Slash.cs
+++ Assets/01_Scripts/Slash.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlashBoss : MonoBehaviour
+public class Slash : MonoBehaviour
 {
     float speed = -15;
     // Start is called before the first frame update
